cab5d07618f4e9f172580efbbe85f140b3e37377
João Vitor Schwingel <jv.schwingel@gmail.com>
2025-03-13 16:00:42 -0300

    config sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ottocanil.com', // Substitua pelo seu domínio real
+  generateRobotsTxt: true, // Gera um arquivo robots.txt automaticamente
+  sitemapSize: 5000
+};
+  
